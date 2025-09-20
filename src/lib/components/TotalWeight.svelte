726eb037dbdfb5c88ef07b5502c5e2a7d9e4bfc1
kevin-vasseure <kevin.vasseure@proton.me>
2025-09-20 12:34:38 +0200

    Use derived store for total weight calculation

--- src/lib/components/TotalWeight.svelte
+++ src/lib/components/TotalWeight.svelte
@@ -1,11 +1,8 @@
 <script lang="ts">
-	import { ingredientsStore } from '../stores/ingredientsStore';
-	import { calculateTotalWeight } from '../utils/calculations';
-
-	$: totalWeight = calculateTotalWeight($ingredientsStore);
+	import { totalWeight } from '../stores/currentRecipeStore';
 </script>
 
-<div class="bg-black px-8 py-6 text-white border-t-2 border-black">
+<div class="border-t-2 border-black bg-black px-8 py-6 text-white">
 	<div class="flex items-center justify-between">
 		<div class="flex items-center">
 			<span class="mr-3 text-2xl">⚖️</span>
@@ -13,9 +10,9 @@
 		</div>
 		<div class="text-right">
 			<div class="text-3xl font-bold">
-				{totalWeight.toFixed(1)}<span class="ml-1 text-xl text-gray-300">grams</span>
+				{$totalWeight}<span class="ml-1 text-xl text-gray-300">grams</span>
 			</div>
-			<div class="text-sm text-gray-300">{(totalWeight / 1000).toFixed(2)} kg</div>
+			<div class="text-sm text-gray-300">{($totalWeight / 1000).toFixed(2)} kg</div>
 		</div>
 	</div>
 </div>
